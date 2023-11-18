e497e2f8110b46ccf0073f75f93a84bfe3acb622
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-19 00:16:32 +0900

    修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,11 +27,11 @@ class AppServiceProvider extends ServiceProvider
 
         // DBにデータ挿入後にコメントアウトを外す
         
-       $page_data =  Page::where('id',1)->first();
-       $room_data = Room::get();
-       $setting_data = Setting::where('id',1)->first();
-       view()->share('global_page_data', $page_data);
-       view()->share('global_room_data', $room_data);
-       view()->share('global_setting_data', $setting_data);
+    //    $page_data =  Page::where('id',1)->first();
+    //    $room_data = Room::get();
+    //    $setting_data = Setting::where('id',1)->first();
+    //    view()->share('global_page_data', $page_data);
+    //    view()->share('global_room_data', $room_data);
+    //    view()->share('global_setting_data', $setting_data);
     }
 }
